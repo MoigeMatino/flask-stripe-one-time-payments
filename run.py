061dc270dda9b4ec656060f2d80d1ed48d633c2e
AgathaBahati <matinodev@gmail.com>
2024-05-31 21:21:23 +0300

    fix: fix error relating to create_app
    - Pass default value to create_app factory method to solve docker error when running migrate

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
+# from flask_migrate import Migrate
 
-app = create_app('default')
+
+app = create_app()
+# migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
